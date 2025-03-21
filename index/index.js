e4dde353179d68182c29cca91699ec7439b93e45
Andrew Chelimo <62815445+DaChelimo@users.noreply.github.com>
2025-03-21 11:29:27 +0300

    Add Gallery

--- index/index.js
+++ index/index.js
@@ -1,17 +1,61 @@
 let openMenuBtn = document.getElementById("openMenuBtn");
 let closeMenuBtn = document.getElementById("closeMenuBtn");
 
-let menu = document.querySelector(".menu")
+let menu = document.querySelector(".menu");
 
+let sliderDots = document.querySelectorAll(".slider-dots li");
+let galleryList = document.querySelectorAll(".gallery-item");
+let activeIndex = 0;
 
-setupMenu()
-
+setupMenu();
+setupGallery();
+setUpGallerySliderDots();
 
 function setupMenu() {
-    openMenuBtn.onclick = () => {
-        menu.style.right = "0px"
-    }
-    closeMenuBtn.onclick = () => {
-      menu.style.right = "-225px";
+  openMenuBtn.onclick = () => {
+    menu.style.right = "0px";
+  };
+  closeMenuBtn.onclick = () => {
+    menu.style.right = "-225px";
+  };
+}
+
+function setupGallery() {
+  galleryList.forEach((element, index) => {
+    element.onclick = () => {
+      activeIndex = index;
+      refreshGallery();
+      refreshSliderDots()
     };
+  });
+}
+
+function refreshGallery() {
+  galleryList.forEach((element, index) => {
+    if (activeIndex == index) {
+      element.classList.add("active");
+    } else {
+      element.classList.remove("active");
+    }
+  });
+}
+
+function setUpGallerySliderDots() {
+  sliderDots.forEach((dot, index) => {
+    dot.onclick = () => {
+      activeIndex = index;
+      refreshGallery()
+      refreshSliderDots();
+    };
+  });
+}
+
+function refreshSliderDots() {
+  sliderDots.forEach((dot, index) => {
+    if (activeIndex == index) {
+      dot.classList.add("active");
+    } else {
+      dot.classList.remove("active");
+    }
+  });
 }
